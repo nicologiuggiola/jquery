48ad71e98707b38575aa4f0378accbd2832c9e74
vivianabobbio <93972864+nicologiuggiola@users.noreply.github.com>
2022-04-05 12:02:30 +0200

    push1

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-$( document ).ready(() => {
-    $('p').on('click', function(){
-        $('p').hide();
-    })
+$(document).ready(() => {
+  // $('p').on('click', function(){
+  //     $('p').hide();
+  // })
+
+  $.getJSON("./assets/array.json", (array) => {
+    $.each(array, (index, value) => {
+      $("#main-container").append("<p>" + value + "</p>");
+    });
+
+    $("p").each(function (index) {
+      $(this).on("click", () => {
+        const text = $(this).text();
+        $("p:contains(" + text + ")").hide();
+      });
+    });
+
+    $("p").each(function (index) {
+      $(this).mouseenter(() => {
+        //const text = $(this).text();
+        $("p:contains(" + $(this).text() + ")").css("color", "green");
+      });
+
+      $(this).mouseleave(() => {
+        //const text = $(this).text();
+        $("p:contains(" + $(this).text() + ")").css("color", "");
+      });
+    });
+  });
 });
 
+// window.addEventListener('load', () => {
 
-window.addEventListener('load', () => {
+//     document.getElementById('ciao').className = "ciao"
 
-    document.getElementById('ciao').className = "ciao"
-    
-    document.getElementById('ciao').classList.add()
-    
-});
+//     document.getElementById('ciao').classList.add()
+
+// });
