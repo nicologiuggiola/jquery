44f44623573621f00bd434def1c9963a23a2b574
labstraction <45163263+labstraction@users.noreply.github.com>
2022-04-05 10:20:46 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$( document ).ready(() => {
+    $('p').on('click', function(){
+        $('p').hide();
+    })
+});
+
+
+window.addEventListener('load', () => {
+
+    document.getElementById('ciao').className = "ciao"
+    
+    document.getElementById('ciao').classList.add()
+    
+});
